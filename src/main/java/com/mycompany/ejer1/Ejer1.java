0000869b0d98456004e803da74ca2f581ae69fa2
Itiel Sanz <Itiel Sanz@DESKTOP-IOJ132C>
2022-09-27 17:45:53 -0500

    Carga 1 27/Sep/2022

---
+++ src/main/java/com/mycompany/ejer1/Ejer1.java
@@ -0,0 +1,13 @@
+package com.mycompany.ejer1;
+
+/**
+ *
+ * @author AXO <l211080078@iztapalapa.tecnm.mx>
+ */
+public class Ejer1 {
+
+    public static void main(String[] args) {
+        Vista ver = new Vista();
+        ver.setVisible(true);
+    }
+}
